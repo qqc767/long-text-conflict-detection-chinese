dee50f18e8fa7425d1e80be2fa4eadc6dd8282f0
lhhhhhe <128984795+qqc767@users.noreply.github.com>
2025-07-04 20:50:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from splitter import split_sentences
 from translator import ChineseToEnglishTranslator
 from nli_detector import DetectConflicts
+import sys
+
+sys.stdout = open('conflict_detection.log', 'w', encoding='utf-8')
 
 def read_text_file(filepath: str) -> str:
     with open(filepath, 'r', encoding='utf-8') as f:
@@ -8,19 +11,19 @@ def read_text_file(filepath: str) -> str:
 
 if __name__ == "__main__":
     # 读取文件内容
-    chinese_text = read_text_file('input_text_3.txt')
+    chinese_text = read_text_file('input_text_2.txt')
 
-    # print("🔹 Step 1: 中文分句")
+    print("🔹 Step 1: 中文分句")
     sentences = split_sentences(chinese_text)
     # print(sentences)
 
-    # print("🔹 Step 2: 翻译为英文")
+    print("🔹 Step 2: 翻译为英文")
     translator = ChineseToEnglishTranslator()
     en_cn_map = translator.translate(sentences)
     # for en, zh in en_cn_map.items():
     #     print(f"[ZH] {zh}\n[EN] {en}\n")
 
-    # print("🔹 Step 3: 进行NLI冲突检测")
+    print("🔹 Step 3: 进行NLI冲突检测")
     # 提取英文句子列表（传给 NLI 检测）
     en_sentences = list(en_cn_map.keys())
 
